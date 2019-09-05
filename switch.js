c8a1e7e4c1ae122f902b531391fcadbb6f0f86d6
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-09-05 16:40:05 -0500

    request-to-postman

--- switch.js
+++ switch.js
@@ -6,9 +6,9 @@ const server = htttp.createServer((request, response)=>{
     const method = request.method
     console.log('method: ', method)
 
-
+/*
     switch(url) {
-        case '/hola':
+        case '/hola':  
             response.write('<h1> HOLA MUNDO  </h1>')
             response.write('<div> <img src = "https://alanxelmundo.com/wp-content/uploads/2019/04/biblioteca.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
         
@@ -23,17 +23,22 @@ const server = htttp.createServer((request, response)=>{
                 
     }
 
-    /*
-    if(url=== '/hola'){
-        response.write('HOLA')
-    } else if ( url === '/adios') {
-        response.write('<h1> ADIOS MUNDO  </h1>')
+    */
+
+    if(url=== '/hola' && method ==='GET') {
+        response.write('HOLA GET')
+    } else if ( url === '/adios' && method === 'POST') {
+        response.write('<h1> ADIOS MUNDO POST  </h1>')
         response.write('<div> <img src = "http://az778189.vo.msecnd.net/media/fotos/g/8d21d5094c7e3df4d7a4b04a6f727859.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
 
-    }else{
-        response.write('<h1> HOLA MUNDO  </h1>')
+    }else if (url === '/' && method ==='PUT' ) {
+        response.write('<h1> HOLA MUNDO PUT </h1>')
         response.write('<div> <img src = "https://alanxelmundo.com/wp-content/uploads/2019/04/biblioteca.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
-    }*/
+    } else {
+        response.write('we cant response you')
+    } 
+
+
     response.end()
 
 } ).listen(4000, () =>{
