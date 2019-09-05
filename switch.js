0eea5902907640936cb7e847cf24513713b32f0e
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-09-05 17:44:26 -0500

    switch-if

--- switch.js
+++ switch.js
@@ -4,9 +4,29 @@ const htttp = require ('http');
 const server = htttp.createServer((request, response)=>{
     const url = request.url
     const method = request.method
-    console.log('method: ', method)
+    console.log('method:', method)
+    console.log('url:', url)
 
-/*
+    switch(url) {
+
+        case '/hola':
+            if (method === 'GET')
+            response.write('<h1> HOLA MUNDO GET  </h1>')
+            response.write('<div> <img src = "https://alanxelmundo.com/wp-content/uploads/2019/04/biblioteca.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
+        break
+
+        case '/adios':
+            if (method === 'GET')
+                response.write('<h1> ADIOS MUNDO  </h1>')
+                response.write('<div> <img src = "http://az778189.vo.msecnd.net/media/fotos/g/8d21d5094c7e3df4d7a4b04a6f727859.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
+        break
+
+        default: 
+        response.write('otra cosa')    
+
+    } 
+
+/*    
     switch(url) {
         case '/hola':  
             response.write('<h1> HOLA MUNDO  </h1>')
@@ -23,10 +43,15 @@ const server = htttp.createServer((request, response)=>{
                 
     }
 
-    */
+*/
+    
+
+    /*
 
     if(url=== '/hola' && method ==='GET') {
-        response.write('HOLA GET')
+        response.write('<h1>HOLA GET </h1>')
+        response.write('<div> <img src = "http://az778189.vo.msecnd.net/media/fotos/g/8d21d5094c7e3df4d7a4b04a6f727859.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
+
     } else if ( url === '/adios' && method === 'POST') {
         response.write('<h1> ADIOS MUNDO POST  </h1>')
         response.write('<div> <img src = "http://az778189.vo.msecnd.net/media/fotos/g/8d21d5094c7e3df4d7a4b04a6f727859.jpg"  style=" height: 100px; width: 150px;"></img> </div>')
@@ -37,6 +62,8 @@ const server = htttp.createServer((request, response)=>{
     } else {
         response.write('we cant response you')
     } 
+
+*/
 
 
     response.end()
